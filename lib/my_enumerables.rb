cf035a7629c1730f9d5f0e2ea72f255b9bb9d35a
Matej <matej.grgin007gmail.com>
2026-04-13 12:33:23 +0200

    add my_each, my_each_with_index, my_select, my_all?

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,11 +1,43 @@
 module Enumerable
-  # Your code goes here
+  def my_each_with_index
+    return to_enum(:my_each_with_index) unless block_given?
+    
+    i = 0
+    my_each do |element|
+      yield(element, i)
+      i += 1
+    end
+    self
+  end
+
+  def my_all?
+    my_each do |element|
+      return false if !yield(element)
+    end
+    true
+  end
+
+
+  def my_select
+      return to_enum(:my_select) unless block_given?
+      
+    result = []
+    my_each do |element|
+      result << element if yield(element)
+    end
+    result
+  end 
 end
 
-# You will first have to define my_each
-# on the Array class. Methods defined in
-# your enumerable module will have access
-# to this method
 class Array
-  # Define my_each here
+  def my_each
+    return to_enum(:my_each) unless block_given?
+    
+    i = 0
+    while i < self.length
+      yield(self[i])
+      i += 1
+    end
+    self
+  end
 end
