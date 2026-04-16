0dd0830f452976675e3bd42c0b86250b1bd80ec9
Matej <matej.grgin007gmail.com>
2026-04-16 12:50:34 +0200

    Add my_any, my_none, my_count, my_map

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -18,6 +18,43 @@ module Enumerable
   end
 
 
+  def my_any?
+    my_each do |element|
+      return true if yield(element)
+    end
+    false
+  end
+
+  def my_none?
+    my_each do |element|
+      return false if yield(element)
+    end
+    true
+  end
+
+  def my_count
+    count = 0
+    my_each do |element|
+      if block_given?
+        count += 1 if yield(element)
+      else
+        count += 1
+      end
+    end
+    count
+  end
+
+  def my_map
+      return to_enum(:my_map) unless block_given?
+
+    result = []
+    my_each do |element|
+      result << yield(element)
+    end
+    result
+  end
+
+
   def my_select
       return to_enum(:my_select) unless block_given?
       
@@ -28,6 +65,7 @@ module Enumerable
     result
   end 
 end
+
 
 class Array
   def my_each
